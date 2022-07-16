51c66df2da2dfb08f0a6dbc1679ed003fa1e309f
Suryansh7058 <83452887+Suryansh7058@users.noreply.github.com>
2022-07-16 23:57:53 +0530

    Loader in Remix

--- app/routes/posts/index.tsx
+++ app/routes/posts/index.tsx
@@ -1,9 +1,47 @@
-type Props = {}
+import { Link, useLoaderData } from '@remix-run/react';
+import { ReactChild, ReactFragment, ReactPortal } from 'react';
+
+type Props = {};
+
+export const loader = () => {
+  const data = {
+    posts: [
+      { id: 1, title: 'Post 1', body: 'This is the first post' },
+      { id: 1, title: 'Post 2', body: 'This is the second post' },
+      { id: 1, title: 'Post 3', body: 'This is the third post' },
+      { id: 1, title: 'Post 4', body: 'This is the fourth post' },
+    ],
+  };
+  return data;
+};
 
 const Posts = (props: Props) => {
+  const { posts } = useLoaderData();
   return (
-    <h1>Posts</h1>
-  )
-}
+    <div>
+      <div className="page-header">
+        <h1>Posts</h1>
+        <Link to="/posts/new-post" className="btn">
+          New Post
+        </Link>
+      </div>
 
-export default Posts
+      <ul className="posts-list">
+        {posts.map(
+          (post: { title: string | null; body: string | null; id: number }) => {
+            return (
+              <li key={post.id}>
+                <Link to={`/posts/${post.title}`}>
+                  <h1>{post.title}</h1>
+                </Link>
+                <p>{post.body}</p>
+              </li>
+            );
+          }
+        )}
+      </ul>
+    </div>
+  );
+};
+
+export default Posts;
